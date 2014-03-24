4877180e888349b03b26df0d8cee590e0b30f3ea
Muhammad Zaman <hambudi@ubuntu.(none)>
2014-03-24 16:54:01 -0700

    a

--- recive.c
+++ recive.c
@@ -53,7 +53,7 @@ int main(){
 		recvfrom(s, buf, buf_size, 0, (struct sockaddr *)&remaddr, &addrlen);
 		if(i==0)
 			time_start = nano_count();
-		if(i%10000 == 0){
+		if(i%100000 == 0){
 			int64_t time_end = nano_count();
 			cout<<"number of packets recived and forewarded "<< i <<endl;
 			cout<<"this took "<<time_end-time_start<<" nanoseconds"<<endl;
